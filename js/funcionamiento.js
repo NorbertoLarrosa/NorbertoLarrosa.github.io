0cd0a3d3507ede3da768b1c220512aedd3e17450
NorbertoLarrosa <84335494+NorbertoLarrosa@users.noreply.github.com>
2021-07-05 11:22:38 -0300

    Add files via upload

---
+++ js/funcionamiento.js
@@ -0,0 +1,98 @@
+window.addEventListener('camera-init', (data) => {
+    console.log('camera-init', data);
+})
+
+window.addEventListener('camera-error', (error) => {
+    console.log('camera-error', error);
+})
+
+AFRAME.registerComponent('registerevents', {
+    init: function () {
+        var marker = this.el;
+        var info = document.getElementById('info-button');
+        let infoV = document.getElementById('infoV');
+        var myAdd;
+        var myImg;
+        var mySound;
+
+        marker.addEventListener('markerFound', function() {
+
+            var markerId = marker.id;
+
+            myAdd = info.getElementsByTagName('a')[0]; //dirección de nueva pagina.
+            myImg = myAdd.getElementsByTagName('input')[0]; // imagen de info.
+
+            if(markerId === "3000" || markerId === "4000")
+            {
+                mySound = marker.object3D.children[0].el.components.sound;
+                marker.addEventListener('click',  function(){
+                    if(marker.object3D.visible)
+                    {
+                        mySound.stopSound();
+                        mySound.playSound();
+                    }
+                })
+            }
+
+            switch(markerId)
+            {
+                case '3000':
+                case '4000':
+                    infoV.innerHTML = 'KULTRUN';
+                    myAdd.setAttribute("href","https://localhost:5000/app/kultrum");
+                    break;
+                case '3100':
+                case '4100':
+                    infoV.innerHTML = 'VASIJA';
+                    myAdd.setAttribute("href","https://localhost:5000/app/vasija");
+                    break;
+                case '3200':
+                case '4200':
+                    infoV.innerHTML = 'HACHA';
+                    myAdd.setAttribute("href","https://localhost:5000/app/hacha");
+                    break;
+                case '3300':
+                case '4300':
+                    infoV.innerHTML = 'MOLINO';
+                    myAdd.setAttribute("href","https://localhost:5000/app/molino");
+                    break;
+                case '3400':
+                case '4400':
+                    infoV.innerHTML = 'PINTURA';
+                    myAdd.setAttribute("href","https://localhost:5000/app/pintura");
+                    break;
+                case '3500':
+                case '4500':
+                    infoV.innerHTML = 'PIPA';
+                    myAdd.setAttribute("href","https://localhost:5000/app/pipa");
+                    break;
+                case '3600':
+                case '4600':
+                    infoV.innerHTML = 'PUNTAS DE FLECHA';
+                    myAdd.setAttribute("href","https://localhost:5000/app/puntas");
+                    break;
+                case '3700':
+                case '4700':
+                    infoV.innerHTML = 'TAPAHUE';
+                    myAdd.setAttribute("href","https://localhost:5000/app/tapahue");
+                    break;
+
+            }
+            
+            myImg.setAttribute("src","/info.png");
+            
+            console.log('Marcador Encontrado');
+            // TODO: Add your own code here to react to the marker being found.
+        });
+
+        marker.addEventListener('markerLost', function() {
+            var markerId = marker.id;
+            console.log('Marcador perdido', markerId);
+
+            myAdd.setAttribute("href","#");
+            myImg.setAttribute("src","/logo2.png");
+
+            infoV.innerHTML = '----------';
+        });
+    }
+});
